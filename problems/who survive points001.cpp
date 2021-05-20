0009ef25d67d82bb1d74bc1a10b56091a652138b
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-20 15:19:06 +0900

    Update who survive points001.cpp

--- problems/who survive points001.cpp
+++ problems/who survive points001.cpp
@@ -1,6 +1,7 @@
+// [3,3,3,3,3] k =3 left = [1,0,0,0] , a[0] =  win , a[1] = lose 
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-  {int N, K, Q, A; cin >> N >> K >> Q;
-  vector<int> P(N, K - Q); while (cin >> A) ++P[--A];
-  for (int p : P) puts(p > 0 ? "Yes" : "No");}
+  {int N, K, Q, A; cin >> N >> K >> Q; // num players 
+  vector<int> P(N, K - Q); while (cin >> A) ++P[--A]; // a[k,k,k,kpoints]
+  for (int p : P) puts(p > 0 ? "Yes" : "No");}// [2>0, -1<0]
