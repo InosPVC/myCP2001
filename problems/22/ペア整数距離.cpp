bbbed2a0e65656a38d169cdc13d0a96551df1d34
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-22 14:25:15 +0900

    Create ペア整数距離.cpp

---
+++ problems/22/ペア整数距離.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i,j,a,b,c,k,d,n,x[10][10];
+int main()
+{
+  for(cin>>n>>d;i<n;i++)for(j=0;j<d;j++)cin>>x[i][j];
+  for(i=0;i<n-1;i++){
+    for(k=1;k<n-i;k++){
+      for(a=0,j=0;j<d;j++)a+=pow(x[i][j]-x[i+k][j],2);
+      b=sqrt(a);if(pow(b,2)==a)c++;
+    }
+  }
+  cout<<c;
+}
