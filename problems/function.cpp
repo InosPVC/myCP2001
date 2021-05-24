6ca8e855a91ac6206fa339320e1c798052efccf6
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-24 14:58:02 +0900

    Update function.cpp

--- problems/function.cpp
+++ problems/function.cpp
@@ -36,3 +36,18 @@ int main(){
   }
   printf("%d",s);
 }
+
+// prime num detector 
+bool isPrime(int n)
+{
+    // Corner case
+    if (n <= 1)
+        return false;
+
+    // Check from 2 to n-1
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
