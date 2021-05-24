1efd21e3182d5564e3ee6a2d22937f1b19b6da50
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-24 23:07:02 +0900

    Update function_mid.cpp

--- problems/function_mid.cpp
+++ problems/function_mid.cpp
@@ -5,3 +5,15 @@ for(int i=1;i<n;i++) ans += (n-1)/i;
 set<string> S; while(n--){ string s; cin>>s; S.insert(s); }
 	for(auto& s:S) if(S.count('!'+s)){ cout<< s <<endl; return 0; } // ?
 	puts("satisfiable");
+
+// (x+y)/2 max 
+sort b
+double t=b[0];
+	for(int i=1;i<n;i++)
+	t=(t+b[i])/2;
+
+//10 4 8 7 3 // a[i-1] < a[i] -> NO // a[i-1] >  a[i] -> YES 
+if(a[i-1]<a[i]){m=std::max(m,t);t=0;}else{t++;}
+
+//attack survival
+https://atcoder.jp/contests/abc141/tasks/abc141_c
