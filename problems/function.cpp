4b4626eba200e9a06587526ad938c82cbed4e957
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-24 14:41:35 +0900

    Update function.cpp

--- problems/function.cpp
+++ problems/function.cpp
@@ -3,3 +3,7 @@ in.erase(remove(in.begin(), in.end(), '!'), in.end()); //remove A from string
 
 // count x++ occurence in string 
 count(a.begin() , a.end(), "x++"); 
+
+// find substring 
+string str = "04037"
+str.find("7") != string::npos -> yes 
