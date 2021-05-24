7cf320aaac005a9185065427aa1ee739f1dc356b
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-24 13:27:07 +0900

    Update mod200集計.cpp

--- problems/23/mod200集計.cpp
+++ problems/23/mod200集計.cpp
@@ -1,15 +1,25 @@
-nt N, A[201010];
-int cnt[202];
-//---------------------------------------------------------------------------------------------------
-void _main() {
-    cin >> N;
-    rep(i, 0, N) cin >> A[i];
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+#include <cmath>
 
-    ll ans = 0;
-    rep(i, 0, N) {
-        int mo = A[i] % 200;
-        ans += cnt[mo];
-        cnt[mo]++;
-    }
-    cout << ans << endl;
+int main()
+{
+	// 6
+	// 123 223 123 523 200 2000
+
+	int n; cin >> n; vector<int>a(n); 
+	rep(i, n){
+		cin >> a[i]; 
+	}
+	long long ans = 0; 
+	vector<int>amari(202);
+	rep(i, n) {
+		int r = a[i] % 200; //123 % 200 = 123 ; 
+		ans += amari[r]; // ans = 0 + amari [0,0,0,0 ,0]
+		amari[r]++; // index 123 amari[123] -> [0,0,0,1,0] 123 番目に1を足す
+
+	}
+	cout << ans; 
 }
