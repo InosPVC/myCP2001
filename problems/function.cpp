76e0a97778719681207dfce4d6ab2d18a1e5268e
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-24 14:35:54 +0900

    Update function.cpp

--- problems/function.cpp
+++ problems/function.cpp
@@ -1,2 +1,5 @@
 // del string contains with !
 in.erase(remove(in.begin(), in.end(), '!'), in.end()); //remove A from string
+
+// count x++ occurence in string 
+count(a.begin() , a.end(), "x++"); 
