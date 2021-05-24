8bb2b9c87791c3604ab29bd29043d38dd134b97f
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-24 12:25:58 +0900

    Update 最大敵倒す予知.cpp

--- problems/21/最大敵倒す予知.cpp
+++ problems/21/最大敵倒す予知.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
- 
+ //2
+//3 5 2
+//4 5
+
 int n;
 long a[100010], b, c, t;
  
@@ -10,9 +13,9 @@ int main(){
 	c = 0;
 	for (int i = 0; i < n; ++i){
 		cin >> b;
-		t = min(b, a[i] + a[i+1]);
-		c += t;
-		a[i+1] -= t - min(b, a[i]);
+		t = min(b, a[i] + a[i+1]); // min(4,3+5)
+		c += t; // c= 4 
+		a[i+1] -= t - min(b, a[i]); // 3 5 2 -> 5 = 4 - min(4,3)
 	}
 	cout << c;
 }
