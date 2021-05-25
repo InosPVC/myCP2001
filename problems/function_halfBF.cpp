22645eef8ddb89608ca938253895c2824ecd63a2
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-25 17:14:20 +0900

    Update function_halfBF.cpp

--- problems/function_halfBF.cpp
+++ problems/function_halfBF.cpp
@@ -1,1 +1,6 @@
-
+// multiplication table reaching destination 
+// 10 -> (2,5) -> (2,5) requires 5 moves 
+for ( ll i =1 ; i*i <=n ; i++)
+   if(n%i != 0 ) continue l; 
+    ll j = n/i; ans = min(ans ,i+j-2)
+      cout << ans ; 
