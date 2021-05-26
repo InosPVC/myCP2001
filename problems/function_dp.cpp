b75ebd8b10467ac2355a3c0e091fb68c0dff8e60
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-26 16:10:46 +0900

    Update function_dp.cpp

--- problems/function_dp.cpp
+++ problems/function_dp.cpp
@@ -6,7 +6,7 @@ https://blog.hamayanhamayan.com/entry/2019/01/08/225228
 
 //all  dp[i] = inf;
 // dp[0] = 0;
-//// chmin ai+1	ai + hi - hi+1
-// chmin ai+1	ai + hi - hi+2
+//// chmin ai+1	,ai + hi - hi+1
+// chmin ai+2	,ai + hi - hi+2
 
 // cout dp[N - 1] //last dp value
