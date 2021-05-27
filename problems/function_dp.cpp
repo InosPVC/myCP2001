a87ff1f618f4223eb9444a2184022e685702e5f3
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-27 12:47:20 +0900

    Update function_dp.cpp

--- problems/function_dp.cpp
+++ problems/function_dp.cpp
@@ -10,3 +10,13 @@ https://blog.hamayanhamayan.com/entry/2019/01/08/225228
 // chmin ai+2	,ai + hi - hi+2
 
 // cout dp[N - 1] //last dp value
+#include<bits/stdc++.h>
+using namespace std;
+int a[100087],dp[100087];
+int main()
+{int n;cin>>n;for(int i=0;i<n;i++)cin>>a[i];dp[0]=0;
+for(int i=1;i<n;i++)
+{dp[i]=dp[i-1]+abs(a[i]-a[i-1]);
+if(i>1)
+ dp[i]=min(dp[i],dp[i-2]+abs(a[i]-a[i-2]));
+}cout<<dp[n-1];}
