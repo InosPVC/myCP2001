c703c6d942679413e88d7683bb7dbf6a08c6a558
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-28 16:54:50 +0900

    Update function_permutation.cpp

--- problems/function_permutation.cpp
+++ problems/function_permutation.cpp
@@ -5,3 +5,18 @@
         cout << s << endl;
     } while (next_permutation(s.begin(), s.end()));
     return 0;
+
+int main() {
+    int n; cin >> n; vector<int> v(n); 
+    for (int i = 0; i < n; i++) cin >> v[i]; 
+     
+
+    do {
+        for (int i = 0; i < n; i++) {
+            cout << v[i];
+            if (i != n-1)cout << " ";
+        }
+        cout << endl;
+    } while (next_permutation(v.begin(), v.end()));
+    return 0;
+}
