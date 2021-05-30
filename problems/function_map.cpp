50a22e8c9a79df31e99b1e61cec151c1cc1af4b0
InosPVC <84431182+InosPVC@users.noreply.github.com>
2021-05-30 13:35:17 +0900

    Update function_map.cpp

--- problems/function_map.cpp
+++ problems/function_map.cpp
@@ -1,1 +1,12 @@
+// how to print a map 
+int n; cin >> n;   map<string, long long> box;
 
+	rep(i, n) {
+		string s; cin >> s; 
+		sort(s.begin(), s.end()); 
+		box[s] ++;
+	}
+
+	for (const auto& x : box) {
+		std::cout << x.first << ": " << x.second << "\n";
+	}
